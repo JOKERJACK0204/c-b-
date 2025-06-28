31b13a51eec6aa01f5cc25fe4cc2df3ddf79cde1
JOKERJACK0204 <captainjackngominhducjj020406@gmail.com>
2025-06-28 13:56:58 +0700

    Add files via upload

---
+++ badman.java
@@ -0,0 +1,16 @@
+public class badman {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Tong cua a va b la: " + c); 
+        int d = a - b;
+        System.out.println("Hieu cua a va b la: " + d);
+        int e = a * b;
+        System.out.println("Tich cua a va b la: " + e);
+        int f = a / b;
+        System.out.println("Thuong cua a va b la: " + f);
+        int g = a % b;
+    }   
+}
